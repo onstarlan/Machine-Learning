a1effb5d8170034282bddfb201a9e91cdd94c2b8
onstarlan <120430038+onstarlan@users.noreply.github.com>
2022-12-15 04:14:49 +0300

    Update Graph of a Sinusoidal function.py

--- Graph of a Sinusoidal function.py
+++ Graph of a Sinusoidal function.py
@@ -1,22 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# In[2]:
-
-
 x = np.arange(-2*np.pi, 2*np.pi, 0.01)
 y = 3*np.sin(4*np.pi-x)
-
-
-# In[3]:
-
 
 plt.plot(x,y,color = "Red")
 plt.xticks([-2*np.pi, -np.pi, 0, np.pi, 2*np.pi],
@@ -26,10 +12,3 @@ plt.yticks([-3,-2,-1,0,+1,+2,+3],
 plt.text(-2*np.pi,2.5,r'$y=3\sin \left( 4\pi -x\right)$',
         fontsize=10,bbox={'facecolor':'blue','alpha':0.2})
 print("www.linkedin.com/onstarlan\nGraph of a Sinusoidal function")
-
-
-# In[ ]:
-
-
-
-
